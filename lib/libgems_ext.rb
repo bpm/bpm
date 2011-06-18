2a2fc4cbe863cbbb6a3dd134c4486a664b90516a
Peter Wagenet <peter.wagenet@gmail.com>
2011-06-17 22:02:46 -0700

    Fixed LibGems sources

--- lib/libgems_ext.rb
+++ lib/libgems_ext.rb
@@ -4,5 +4,8 @@ require 'libgems_ext/dependency_installer'
 require 'libgems_ext/installer'
 require 'libgems_ext/spec_fetcher'
 
+# Empty sources to make sure we don't get ones from RubyGems
+LibGems.sources = nil
+
 # Reload from new paths
 LibGems.load_configuration
