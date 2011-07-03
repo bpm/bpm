53d04f9377f53b1a1b02fac2827d2b622f7dbcf5
Charles Jolley <charles@sproutcore.com>
2011-07-03 00:17:51 -0700

    Move some specs into their own directories.

---
+++ spec/plugins/transport_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+describe BPM::Pipeline, 'transport processor' do
+
+  before do
+    goto_home
+    set_host
+    reset_libgems bpm_dir.to_s
+    start_fake(FakeGemServer.new)
+    
+    FileUtils.cp_r fixtures('transporter'), '.'
+    cd home('transporter')
+
+    bpm 'compile'
+    wait
+  end
+  
+  subject do
+    project = BPM::Project.new home('transporter')
+    BPM::Pipeline.new project
+  end
+  
+  it "should wrap the project's main.js" do
+    asset = subject.find_asset 'transporter/lib/main.js'
+    exp_path = home('transporter', 'lib', 'main.js')
+    asset.to_s.should == "define_transport(function() {\n//TRANSPORT\ntransporter();\n//TRANSPORT\n\n}), 'transporter', 'main', '#{exp_path}');\n\n"
+    asset.pathname.to_s.should == File.join(Dir.pwd, 'lib', 'main.js')
+  end
+
+  it "should not wrap transport/main.js" do
+    asset = subject.find_asset 'transport/lib/main.js'
+    asset.to_s.should == "// TRANSPORT DEMO\n"
+  end
+
+end
