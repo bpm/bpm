3b06f9f1e61ce3a11ebe524360b34b34beedcc82
Peter Wagenet <peter.wagenet@gmail.com>
2011-08-25 14:40:35 -0700

    Whitespace

--- lib/bpm/server.rb
+++ lib/bpm/server.rb
@@ -4,20 +4,20 @@ require 'sprockets'
 module BPM
 
   class Server < Rack::Server
-    
+
     def initialize(project, options=nil)
       @project = project
-      @mode    = (options && options[:mode]) || :debug 
+      @mode    = (options && options[:mode]) || :debug
       super options
     end
-    
+
     def self.start(project, options=nil)
       new(project, options).start
     end
-    
+
     attr_reader :project
     attr_reader :mode
-    
+
     def app
       cur_project = @project
       cur_mode    = @mode
@@ -26,13 +26,11 @@ module BPM
         map '/assets' do
           run BPM::Pipeline.new cur_project, cur_mode, true
         end
-        
+
         map '/' do
           run Rack::Directory.new cur_project.root_path
         end
-        
       end.to_app
     end
-    
   end
 end
