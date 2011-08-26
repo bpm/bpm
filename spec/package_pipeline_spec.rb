afede3e49d4d9cf2885c5fd4231a18bef279dc89
Peter Wagenet <peter.wagenet@gmail.com>
2011-08-25 19:15:37 -0700

    Fix issue with formats being incorrectly wrapped in transports - Fixes #28

--- spec/package_pipeline_spec.rb
+++ spec/package_pipeline_spec.rb
@@ -17,7 +17,10 @@ describe BPM::PackagePipeline do
 
   it "should get package pipelines for each package" do
     names = subject.package_pipelines.map { |pipeline| pipeline.package.name }
-    names.sort.should == %w(coffee coffee-script handlebars spade)
+    # NOTE: We handlebars is so deeply nested to test a very specific issue with
+    # formats that are re-provided at least twice by packages that have a transport.
+    # There is currently a fix for this in BPM::PluginProcessor
+    names.sort.should == %w(coffee coffee-script handlebars handlebars-format spade spade-handlebars)
   end
   
   it "should get an asset for the coffee file" do
